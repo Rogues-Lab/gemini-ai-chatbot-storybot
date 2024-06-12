21ac6ad4a11fbae4a8043520f2250d1d1322ec70
Darren Rogan <darren.rogan@gmail.com>
2024-06-12 20:21:32 +1000

    updated replicate

--- app/api/prediction/infer/route.ts
+++ app/api/prediction/infer/route.ts
@@ -1,50 +1,29 @@
-// 'use server' 
-export async function GET() {
+'use server' 
+import { getAsyncComic, getPrediction } from './replicate';
 
-  const data = { message: 'Request processed successfully.'};
+export async function GET(req: Request) {
+
+  const url = new URL(req.url as string);
+  const reference = url.searchParams.get('reference');
+  console.log('reference', reference);
+  
+  if(!reference){
+    return Response.json('Reference is required', { status: 500 });
+  }
+  const prediction = await getPrediction(reference as string);
  
-  return Response.json({ data })
+  return Response.json({ prediction })
 }
 
-// import { type NextRequest } from 'next/server'
-// import { createJob, getAsyncComic, getPrediction } from './replicate';
+export async function POST(req: Request) {
+  const { comic_description, character_description } = await req.json()
 
-  
-// export async function GET(request: Request) {
-//         const { searchParams } = new URL(request.url)
-//         const reference = searchParams.get('reference')
+    
+  if(!comic_description || !character_description){
+    return Response.json('comic_description and character_description is required', { status: 500 });
+  }
 
-//         // const job = getJob(reference as string);
-//         const prediction = await getPrediction(reference as string);
-//         let res = Response
-//         if(prediction){
-//             res.json(prediction);
-//         } else {
-//             res.json({ message: 'Job not found' });
-//         }
-
-//     return res;
-//     // } else if (req.method === 'POST') {
-//     //     // Handle POST request
-//     //     const { body } = req;
-
-
-//     //     // Process the request body
-//     //     const { reference, comic_description, character_description } = body;
-//     //     const inferJob = await createJob(reference, comic_description, {character_description: character_description});
-
-//     //     const comic = await getAsyncComic(comic_description, character_description);
-//     //     console.log('comic', comic);
-      
-        
-//     //     if(inferJob){
-//     //         res.status(200).json(inferJob);
-//     //     } else {
-//     //         res.status(404).json({ message: 'Job not found' });
-//     //     }
-//     //     res.status(200).json({ message: 'Request processed successfully.' });
-//     // } else {
-//         // Handle other request methods
-//         // res.status(405).json({ message: 'Method Not Allowed' });
-//     // }
-// }
+  const comic = await getAsyncComic(comic_description, character_description);
+  console.log('comic', comic);
+  return Response.json({ comic })
+}
--- components/media/comic.tsx
+++ components/media/comic.tsx
@@ -1,80 +1,138 @@
 'use client'
-import { use, useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react'
 import { SpinnerIcon } from '../ui/icons'
-import { getAsyncComic, getPrediction } from '../../app/api/prediction/infer/replicate';
+import { Button } from '../ui/button'
 
+export const Comic = ({
+  characterDescription,
+  comicGenerationPrompt
+}: {
+  characterDescription: string
+  comicGenerationPrompt: string
+}) => {
+  console.log('Comic component', characterDescription, comicGenerationPrompt)
 
-export const Comic = ({characterDescription, comicGenerationPrompt }: {characterDescription :string,  comicGenerationPrompt: string }) => {
-  console.log('Comic component', characterDescription, comicGenerationPrompt);
-
-  const [comic, setComic] = useState<any>(null);
-  const [replicateJob, setReplicateJob] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [editableCharacterDescription, setEditableCharacterDescription] =
+    useState(characterDescription)
+  const [editableComicGenerationPrompt, setEditableComicGenerationPrompt] =
+    useState(comicGenerationPrompt)
 
   useEffect(() => {
+    setEditableCharacterDescription(characterDescription)
+    setEditableComicGenerationPrompt(comicGenerationPrompt)
+    setComic(null)
+    setReplicateJob(null)
+    setLoading(false)
+  }, [characterDescription, comicGenerationPrompt])
 
-    const generateComic = async () => {
-      try {
-        const predictionJob = await getAsyncComic(comicGenerationPrompt, characterDescription);
-        await setReplicateJob(predictionJob);
-        console.log('replicateJob', predictionJob);
-        await pollPrediction(predictionJob?.id);
-      } catch (error) {
-        console.error('Error generating comic', error);
+  const [comic, setComic] = useState<any>(null)
+  const [replicateJob, setReplicateJob] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const generateComic = async () => {
+    try {
+      const api_post_url = '/api/prediction/infer/'
+      const response = await fetch(api_post_url, {
+        method: 'POST',
+        body: JSON.stringify({
+          comic_description: editableCharacterDescription,
+          character_description: editableComicGenerationPrompt
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      const data = await response.json()
+      console.log('data', data)
+
+      if (data?.comic) {
+        setLoading(true)
+        setComic(data.comic)
+        setReplicateJob(data.comic.id)
+        setLoading(false)
+        setTimeout(() => pollPrediction(replicateJob), 2000)
+      } else if (data?.error) {
+        setLoading(false)
+        console.error('Error generating comic', data.error)
+      } else {
+        setTimeout(() => pollPrediction(replicateJob), 2000)
       }
+    } catch (error) {
+      console.error('Error generating comic', error)
     }
-    generateComic();
-
-  }, [comicGenerationPrompt, characterDescription]);
+  }
 
   const pollPrediction = async (reference: string) => {
+    setLoading(true)
 
-    setLoading(true);
-    const prediction = await getPrediction(reference);
-    console.log('pollPrediction', prediction);
-    if (prediction?.output?.comic) {
-      setComic(prediction?.output?.comic);
-      setLoading(false);
-    } else if (prediction?.error) {
-      setLoading(false);
-      console.error('Error generating comic', prediction.error);
-    } else {
-      setTimeout(() => pollPrediction(reference), 2000);
+    const api_get_url = '/api/prediction/infer/reference=' + reference
+    const response = await fetch(api_get_url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log('poll data', data);
+
+    if (data?.prediction?.status === 'done') {
+      setLoading(false)
+      setComic(data.comic)
     }
   }
 
   return (
     <div className="flex flex-col gap-2">
-         {!comic && 
-          <div className="flex flex-col items-center text-black">
-            <div className="text-black text-sm">Comic generation is in progress...</div>
-              <div
-                className={`flex flex-row  items-center ${loading ? 'opacity-100' : 'opacity-0'}`}
-              >
-                <SpinnerIcon />
-                
-
+      <div className="flex flex-col items-center text-black">
+        {loading && (
+          <>
+            <div className="text-black text-sm">
+              Comic generation is in progress...
             </div>
-            <div className="flex flex-col items-center">
-              <div className="text-black text-sm">characterDescription... {characterDescription}</div>
-              <div className="text-black text-sm">comicGenerationPrompt... {comicGenerationPrompt}</div>
-              <div className="text-black text-sm">comic... {comic}</div>
-              <div className="text-black text-sm">isLoading... {loading == true ? 'true' : 'false'}</div>
-              <div className="text-black text-sm">replicateJob... {replicateJob}</div>
+            <div
+              className={`flex flex-row  items-center ${loading ? 'opacity-100' : 'opacity-0'}`}
+            >
+              <SpinnerIcon />
             </div>
+          </>
+        )}
+        <div className="flex flex-col items-center">
+          <div className="text-black text-sm">
+            characterDescription:
+            <input
+              type="text"
+              value={characterDescription}
+              onChange={e => setEditableCharacterDescription(e.target.value)}
+            />
           </div>
-         }
+          <div className="text-black text-sm">
+            comicGenerationPrompt:
+            <input
+              type="text"
+              value={comicGenerationPrompt}
+              onChange={e => setEditableComicGenerationPrompt(e.target.value)}
+            />
+          </div>
+          <div className="text-black text-sm">
+            isLoading... {loading == true ? 'true' : 'false'}
+          </div>
+          <div className="text-black text-sm">
+            replicateJob... {replicateJob}
+          </div>
+        </div>
+        <Button onClick={generateComic} className="mt-2 bg-blue-700 text-white">
+          Generate Comic
+        </Button>
+      </div>
 
-         {comic &&
-            <div className="flex flex-row items-center">
-              <div className="text-black text-sm">Comic generation is in done...</div>
-       
-                <img src={comic.output} alt="comic" className="w-32 h-32" />
+      {comic && (
+        <div className="flex flex-row items-center">
+          <div className="text-black text-sm">Comic generation is done...</div>
 
-                <div className="text-black text-sm">Comic... ${comicGenerationPrompt}</div>
-
-            </div>
-          }
+          <img src={comic.output?.comic} alt="comic" width={500} height={500} />
+        </div>
+      )}
     </div>
   )
 }
